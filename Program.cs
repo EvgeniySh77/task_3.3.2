4ef2e23b081119fc20d43e395d18d29544a812b3
EvgeniySh77 <evgen.sh77@mail.ru>
2021-12-25 21:32:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task_3._3._2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadLine();
+        }
+    }
+}
